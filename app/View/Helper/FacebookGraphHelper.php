34738d489cd8f1af39f2b5732633e6a937f5c7c1
Russell Porter <contact@russellporter.com>
2012-04-19 18:56:22 +0000

    Update Facebook API

--- app/View/Helper/FacebookGraphHelper.php
+++ app/View/Helper/FacebookGraphHelper.php
@@ -25,13 +25,13 @@ class FacebookGraphHelper extends AppHelper {
         if(isset($config['sources'][$source])) {
             $html = '<div class="facebook">
 		<div id="fb-root"></div>
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, \'script\', \'facebook-jssdk\'));</script>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, \'script\', \'facebook-jssdk\'));</script>
 
       <div class="fb-like-box" data-href="'.$config['sources'][$source].'" data-width="292" data-show-faces="false" data-stream="false" data-header="false"></div>
       </div>';
