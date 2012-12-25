7be41f32738769df4a48e305ee50a9dc50601d44
Russell Porter <contact@russellporter.com>
2012-12-25 02:36:34 +0000

    #46: New link helper

---
+++ app/View/Helper/LinkHelper.php
@@ -0,0 +1,27 @@
+<?php
+class LinkHelper extends Helper {
+    public $helpers = array('Html');
+    private $endpoints = array(
+        'Course' => '/courses/map/',
+        'Map' => '/maps/rendering/',
+        'Result' => '/events/rendering/',
+    );
+
+    // Build the URL to an event (may not be at the same club)
+    // requires Club.id, and Club.domain, as well as Event.id
+    public function eventURL($event, $club) {
+        if(empty($club['id']) || empty($club['domain']) || empty($event['id'])) {
+            return null;
+        }
+        
+        $domain = $club['domain'];
+        $id = $event['id'];
+        $path = "/Events/view/$id";
+        if($club['id'] === Configure::read('Club.id')) {
+            return 	$path;
+        }
+        
+        return "http://".$domain.$path;
+    }
+}
+?>
