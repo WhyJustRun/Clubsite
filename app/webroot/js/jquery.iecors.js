2f265e8e189fe0239bdffa9dfe463a7f3c1cf024
Russell Porter <contact@russellporter.com>
2013-02-27 05:54:34 +0000

    Use a better IE CORS polyfill that doesn't break randomly

--- app/webroot/js/jquery.iecors.js
+++ app/webroot/js/jquery.iecors.js
@@ -1,47 +1,88 @@
-(function( jQuery ) {
-  // Create the request object
-  // (This is still attached to ajaxSettings for backward compatibility)
-  jQuery.ajaxSettings.xdr = function() {
-    return (window.XDomainRequest ? new window.XDomainRequest() : null);
-  };
+// Coded to Microsoft XDR spec: http://msdn.microsoft.com/en-us/library/cc288060(v=vs.85).aspx
 
-  // Determine support properties
-  (function( xdr ) {
-    jQuery.extend( jQuery.support, { iecors: !!xdr });
-  })( jQuery.ajaxSettings.xdr() );
+(function($) {
+    "use strict";
+    
+    // Create the request object
+    // (This is still attached to ajaxSettings for backward compatibility)
+    $.ajaxSettings.xdr = function() {
+        return (window.XDomainRequest ? new window.XDomainRequest() : null);
+    };
 
-  // Create transport if the browser can provide an xdr
-  if ( jQuery.support.iecors ) {
+    // Determine support properties
+    (function(xdr) {
+        $.extend($.support, { iecors: !!xdr });
+    })(window.XDomainRequest);
 
-    jQuery.ajaxTransport(function( s ) {
-      var callback;
+    // Create transport if the browser can provide an xdr (and fails $.support.cors)
+    if ($.support.iecors) {
 
-      return {
-        send: function( headers, complete ) {
-          var xdr = s.xdr();
+        console.log("Adding support for iecors");
 
-          xdr.onload = function() {
-            var headers = { 'Content-Type': xdr.contentType };
-            complete(200, 'OK', { text: xdr.responseText }, headers);
-          };
-          
-          // Apply custom fields if provided
-					if ( s.xhrFields ) {
-            xhr.onerror = s.xhrFields.error;
-            xhr.ontimeout = s.xhrFields.timeout;
-					}
+        $.ajaxTransport(function(options, originalOptions, jqXHR) {
+            var xdr;
+            
+            return {
+                send: function(headers, complete) {
+                    xdr = options.xdr();
 
-          xdr.open( s.type, s.url );
+                    // XDR does not support custom headers
+                    
+                    // Seems that xdr requests can get hung up indefinitely without a timeout.
+                    xdr.timeout = options.timeout || 10000;
 
-          // XDR has no method for setting headers O_o
+                    // IE9 has a bug that requires the xdr.onprogress method to be set. We'll just set them all, just in case.
+                    // (http://social.msdn.microsoft.com/Forums/en-US/iewebdevelopment/thread/30ef3add-767c-4436-b8a9-f1ca19b4812e)
+                    xdr.onload = xdr.onerror = xdr.ontimeout = xdr.onprogress = $.noop;
+                    
+                    xdr.onload = function() {
+                        console.log("XDR load");
+                        var headers = {
+                            'Content-Type': xdr.contentType
+                        };
+                        
+                        complete(200, 'OK', { text: xdr.responseText }, headers);
+                    };
 
-          xdr.send( ( s.hasContent && s.data ) || null );
-        },
+                    if (options.xhrFields) {
+                        if (options.xhrFields.progress) xhr.onprogress = options.xhrFields.progress;
+                        if (options.xhrFields.error) xhr.onerror = options.xhrFields.error;
+                        if (options.xhrFields.timeout) xhr.ontimeout = options.xhrFields.timeout;
+                        // XDR does not support withCredentials
+                    } else {
+                        xdr.onprogress = function() {
+                            console.log("XDR progress");
+                        };
+                        xdr.onerror = function() {
+                            console.log("XDR error");
+                            complete(404, "Not Found");
+                        };
+                        xdr.ontimeout = function() {
+                            console.log("XDR timeout");
+                            complete(408, "Request Timeout");
+                        };
+                    }
+                    
+                    // // TODO: If you're getting "Aborted" requests in IE9, try uncommenting this block.
+                    // // A few people reported 'jQuery.noop' wasn't good enough, but I can't figure out why.
+                    // xdr.onprogress = function() {
+                    //     console.log("XDR progress");
+                    // };
 
-        abort: function() {
-          xdr.abort();
-        }
-      };
-    });
-  }
-})( jQuery );
+                    xdr.open(options.type, options.url);
+                    
+                    if (options.hasContent && options.data) {
+                        xdr.send(options.data);
+                    } else {
+                        xdr.send();
+                    }
+                },
+
+                abort: function () {
+                    console.log("XDR abort");
+                    return xdr && xdr.abort();
+                }
+            };
+        });
+    }
+})(jQuery);
