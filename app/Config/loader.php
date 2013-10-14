a126e1892f2d8d0e0473cac72f16924cf1e4e269
Russell Porter <contact@russellporter.com>
2013-10-14 17:27:18 +0000

    Fixes for the loader

--- app/Config/loader.php
+++ app/Config/loader.php
@@ -2,40 +2,47 @@
 // Responsible for loading the club config into CakePHP
 App::import('Model', 'ConnectionManager');
 App::uses('Sanitize', 'Utility');
-function clubFromHost($host) {
-    $escapedHost = Sanitize::escape($host, 'default');
-    clubForQuery("SELECT * FROM clubs WHERE domain = '$escapedHost' OR domain = 'www.$escapedHost'", false);
-}
-
-function clubFromID($id) {
-    $id = intval($id);
-    clubForQuery("SELECT * FROM clubs WHERE id = $id", true);
-}
 
 function validClubResults($results) {
     return (count($results) === 1) && (!empty($results[0]["clubs"]));
 }
 
-function clubForQuery($query, $fromID) {
+function clubFromHost($host) {
+    $query = "SELECT * FROM clubs WHERE domain = ?";
     $db = ConnectionManager::getDataSource('default');
-    $results = $db->fetchAll($query);
+    $results = $db->fetchAll($query, array($host));
     if (validClubResults($results)) {
-        foreach($results[0]['clubs'] as $key => $value) {
+        foreach ($results[0]['clubs'] as $key => $value) {
             if ($key === 'timezone') {
                 $value = timezone_open($value);
             }
             Configure::write('Club.'.$key, $value);
         }
-    } else if ($fromID) {
-        die("ERROR: No valid club results.");
     } else {
-        clubFromID(1);
-    }
+        // See if it is a redirect domain
+        $redirectQuery = "SELECT * FROM clubs WHERE redirect_domain = ?";
+        $results = $db->fetchAll($redirectQuery, array($host));
+        if (validClubResults($results)) {
+            $targetHost = $results[0]['clubs']['domain'];
+            redirectHostPermanentlyTo($targetHost);
+        } else {
+            die("ERROR: Loading club failed. Please contact support@whyjustrun.ca and include the URL you were requesting.");
+        }
+    } 
+}
+
+function redirectHostPermanentlyTo($targetHost) {
+    header("HTTP/1.1 301 Moved Permanently");
+    $requestURL = $_SERVER["REQUEST_URI"];
+    header("Location: http://" . $targetHost . $requestURL);
+    exit();
 }
 
 // Load in club configuration based on the request domain name
 if (!empty($_SERVER['HTTP_HOST'])) {
     clubFromHost($_SERVER['HTTP_HOST']);
 } else {
-    clubFromID(1);
+    // Allow command line scripts to still work
+    clubFromHost("gvoc.whyjustrun.ca");
 }
+
