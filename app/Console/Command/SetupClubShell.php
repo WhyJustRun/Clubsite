77173d7db08d5ac2a15a1b6954f6eecaf61af638
Russell Porter <contact@russellporter.com>
2013-10-30 17:35:06 +0000

    Updates to setup club shell

--- app/Console/Command/SetupClubShell.php
+++ app/Console/Command/SetupClubShell.php
@@ -11,21 +11,28 @@ class SetupClubShell extends Shell {
         $club['url'] = $this->in('URL?');
         $club['lat'] = $this->in('Latitude?');
         $club['lng'] = $this->in('Longitude?');
+        $club['country'] = $this->in('Country?');
         $club['timezone'] = $this->in('Timezone?');
-            
+        $club['visible'] = $this->in('Visible (0/1)?');
+        $club['domain'] = $this->in('Domain?');
+        $club['redirect_domain'] = $this->in('Redirect domain?');
+        $club['parent_id'] = $this->in('Parent club id?');
+        $club['club_category_id'] = $this->in('Club category id?');
+        $club['facebook_page_id'] = $this->in('Facebook page id?');
+
         $this->Club->save(array('Club' => $club));
         $club['id'] = $this->Club->id;
         
         $this->out('Club created! Add an administrator.');
-        $adminUsername = $this->in('Username?');
+        $adminEmail = $this->in('Email address?');
         
-        $user = $this->User->findByUsername($adminUsername);
+        $user = $this->User->findByEmail($adminEmail);
         $this->Group->save(array('Group' => array('name' => 'Administrator', 'access_level' => 100, 'club_id' => $this->Club->id)));
         $groupId = $this->Group->id;
         if($user != null) {
             $this->Privilege->save(array('Privilege' => array('group_id' =>  $groupId, 'user_id' => $user['User']['id'])));
         }
         
-        $this->out('All done. Head to '.$club['url'].' and get the club up and running!');
+        $this->out('All done. Head to '.$club['domain'].' and get the club up and running!');
     }
 }
