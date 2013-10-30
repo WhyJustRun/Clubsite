833f357a310c2f6c10d11fbcc5bae713a4858192
Russell Porter <contact@russellporter.com>
2013-10-30 18:39:15 +0000

    Handle club validation failure

--- app/Console/Command/SetupClubShell.php
+++ app/Console/Command/SetupClubShell.php
@@ -1,7 +1,7 @@
 <?php
 class SetupClubShell extends Shell {
     public $uses = array('Club', 'User', 'Group', 'Privilege');
-    
+
     public function main() {
         $this->out('Enter club information:');
         $club['name'] = $this->in('Name?');
@@ -21,24 +21,29 @@ class SetupClubShell extends Shell {
         $club['facebook_page_id'] = $this->in('Facebook page id?');
 
         $this->Club->create();
-        $this->Club->save(array('Club' => $club));
-        $club['id'] = $this->Club->id;
-        
-        $this->out('Club created! Add a webmaster:');
-        
-        $adminEmail = $this->in('Email address?');
-        
-        $user = $this->User->findByEmail($adminEmail);
-        $this->Group->create();
-        $this->Group->save(array('Group' => array('name' => 'Executive', 'description' => 'Add/modify maps, events, organizers, resources.', 'access_level' => 80, 'club_id' => $this->Club->id)));
-        $this->Group->create();
-        $this->Group->save(array('Group' => array('name' => 'Webmaster', 'description' => 'Club webmasters can edit privileges', 'access_level' => 90, 'club_id' => $this->Club->id)));
-        $groupId = $this->Group->id;
-        if($user != null) {
-            $this->Privilege->create();
-            $this->Privilege->save(array('Privilege' => array('group_id' =>  $groupId, 'user_id' => $user['User']['id'])));
-        }
+        if ($this->Club->save(array('Club' => $club))) {
+            $club['id'] = $this->Club->id;
 
-        $this->out('All done. Head to '.$club['domain'].' and get the club up and running!');
+
+            $this->out('Club created! Add a webmaster:');
+
+            $adminEmail = $this->in('Email address?');
+
+            $user = $this->User->findByEmail($adminEmail);
+            $this->Group->create();
+            $this->Group->save(array('Group' => array('name' => 'Executive', 'description' => 'Add/modify maps, events, organizers, resources.', 'access_level' => 80, 'club_id' => $this->Club->id)));
+            $this->Group->create();
+            $this->Group->save(array('Group' => array('name' => 'Webmaster', 'description' => 'Club webmasters can edit privileges', 'access_level' => 90, 'club_id' => $this->Club->id)));
+            $groupId = $this->Group->id;
+            if($user != null) {
+                $this->Privilege->create();
+                $this->Privilege->save(array('Privilege' => array('group_id' =>  $groupId, 'user_id' => $user['User']['id'])));
+            }
+
+            $this->out('All done. Head to '.$club['domain'].' and get the club up and running!');
+        } else {
+            debug($this->Club->validationErrors);
+            $this->out('Invalid input - club validation failed. Try again');
+        }
     }
 }
