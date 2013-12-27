e875b50ee2fdcc8be6186d5caa5b53dc846b1541
Russell Porter <contact@russellporter.com>
2013-12-27 01:15:20 +0000

    Oops missed some files.. Fixes #127 (remove unexposed series page)

--- app/Model/Series.php
+++ app/Model/Series.php
@@ -42,43 +42,4 @@ class Series extends AppModel {
                 'counterQuery' => ''
                 )
             );
-
-    function findOrganizers($id) {
-        $events = $this->Event->findAllBySeriesId($id);
-        foreach($events as $event) {
-            $eventId = $event["Event"]["id"];
-            $org = $this->Event->Organizer->findAllByEventId($eventId);
-            foreach($org as $or) {
-                $userId = $or["Organizer"]["user_id"];
-                $organizers[$userId]["name"] = $or["User"]["name"];
-                $organizers[$userId]["id"] = $or["User"]["id"];
-                $organizers[$userId]["role"] = $or["Role"]["name"];
-            }
-        }
-        return $organizers;
-    }
-    function startDate($id) {
-        $events = $this->Event->findAllBySeriesId($id, array('date'));
-        if(count($events) == 0)
-            return NULL;
-        $startDate = 999999999999999;//$events[0]["Event"]["date"];
-        foreach($events as $event) {
-            if($event["Event"]["date"] < $startDate)
-                $startDate = $event["Event"]["date"] ;
-        }
-
-        return $startDate;
-    }
-    function endDate($id) {
-        $events = $this->Event->findAllBySeriesId($id, array('date'));
-        if(count($events) == 0)
-            return NULL;
-        $endDate = -999999999999999;
-        foreach($events as $event) {
-            if($event["Event"]["date"] > $endDate)
-                $endDate = $event["Event"]["date"] ;
-        }
-        return $endDate;
-    }
-
 }
