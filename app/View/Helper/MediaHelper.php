10524e41896d6958244042ccc4b0c7f3a0482600
Russell Porter <contact@russellporter.com>
2014-01-29 06:57:02 +0000

    Ok.. first polyfill was buggy :/ this one seems to work great though

--- app/View/Helper/MediaHelper.php
+++ app/View/Helper/MediaHelper.php
@@ -8,7 +8,7 @@ class MediaHelper extends Helper {
     );
 
     public function image($type, $id, $thumbnail = false, $options = array()) {
-        $options['srcset'] = $this->url($type, $id, $thumbnail, true) . " 2x";
+        $options['srcset'] = $this->url($type, $id, $thumbnail) . ' 1x, ' . $this->url($type, $id, $thumbnail, true) . " 2x";
         return $this->Html->image($this->url($type, $id, $thumbnail), $options);
     }
 
