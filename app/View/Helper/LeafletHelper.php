4e03d4e4d58341ce6472b99cce1e393fdd893947
Russell Porter <contact@russellporter.com>
2014-01-31 03:06:37 +0000

    Fix regression in map interaction on mobile

--- app/View/Helper/LeafletHelper.php
+++ app/View/Helper/LeafletHelper.php
@@ -123,6 +123,7 @@ class LeafletHelper extends AppHelper {
                     // disable interaction handlers that impact scrolling
                     $script .= 'map.dragging.disable();
                     map.scrollWheelZoom.disable();
+                    map.tap.disable();
                     ';
                 }
 
