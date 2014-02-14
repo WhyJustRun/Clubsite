9ac5769bfb4bb1e978f56b84369f26a87d3557f4
Russell Porter <contact@russellporter.com>
2014-02-14 03:43:01 +0000

    Use fixed toolbar sometimes for redactor

--- app/webroot/js/wjr-text-editor.redactor.js
+++ app/webroot/js/wjr-text-editor.redactor.js
@@ -3,5 +3,10 @@ function orienteerAppWYSIWYGUpdateTextareas() {
 
 // hack. does async loading of the wysiwyg editor
 function orienteerAppWYSIWYG(element) {
-    $(element).redactor();
+    // If we are not on a jeditable textarea, use the fixed toolbar. There are bugs with jeditable and fixed toolbar opening as of Feb 2014.
+    if ($(element).parent().parent().hasClass('editable')) {
+        $(element).redactor();
+    } else {
+        $(element).redactor({ toolbarFixed: true, toolbarFixedBox: true });
+    }
 }
