d2f64a6f5f42c4605d373c8fa90df76e5fb84875
Russell Porter <contact@russellporter.com>
2014-02-14 03:45:00 +0000

    Actually never mind, this always happens :/

--- app/webroot/js/wjr-text-editor.ckeditor.js
+++ app/webroot/js/wjr-text-editor.ckeditor.js
@@ -2,7 +2,6 @@ function orienteerAppWYSIWYGUpdateTextareas() {
     _.invoke(CKEDITOR.instances, 'updateElement');
 }
 
-// hack. does async loading of the wysiwyg editor
 function orienteerAppWYSIWYG(element) {
     CKEDITOR.replace($(element).attr('name'));
 }
--- app/webroot/js/wjr-text-editor.redactor.js
+++ app/webroot/js/wjr-text-editor.redactor.js
@@ -1,12 +1,6 @@
 function orienteerAppWYSIWYGUpdateTextareas() {
 }
 
-// hack. does async loading of the wysiwyg editor
 function orienteerAppWYSIWYG(element) {
-    // If we are not on a jeditable textarea, use the fixed toolbar. There are bugs with jeditable and fixed toolbar opening as of Feb 2014.
-    if ($(element).parent().parent().hasClass('editable')) {
-        $(element).redactor();
-    } else {
-        $(element).redactor({ toolbarFixed: true, toolbarFixedBox: true });
-    }
+    $(element).redactor({ toolbarFixed: true, toolbarFixedBox: true });
 }
