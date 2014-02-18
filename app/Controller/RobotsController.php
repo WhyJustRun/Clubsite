7bb2b0a6c6b8d2ed328fed0e89c8c025216e4901
Russell Porter <contact@russellporter.com>
2014-02-18 04:23:23 +0000

    You shouldn't need to sign in to access robots.txt :P

--- app/Controller/RobotsController.php
+++ app/Controller/RobotsController.php
@@ -4,6 +4,12 @@ class RobotsController extends AppController {
 
     var $name = 'Robots';
 
+    public function beforeFilter() {
+        parent::beforeFilter();
+
+        $this->Auth->allow('view');
+    }
+
     public function view() {
         $this->layout = null;
         $this->set('hidden', (Configure::read('debug') === 1 || Configure::read('Club.visible') === false));
