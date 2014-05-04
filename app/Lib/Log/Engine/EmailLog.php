f56221bdaaf5b8268791199c72a29b2ad8472ca3
Russell Porter <contact@russellporter.com>
2014-05-04 13:36:13 -0700

    Fix error when there are no emails to send logs to

--- app/Lib/Log/Engine/EmailLog.php
+++ app/Lib/Log/Engine/EmailLog.php
@@ -4,30 +4,33 @@ App::uses('CakeEmail', 'Network/Email');
 
 class EmailLog implements CakeLogInterface {
     function __construct($options = array()) {
-        
+
     }
 
     function write($type, $error) {
-        $email = new CakeEmail('default');
-        $email->emailFormat('text');
-        $email->from(array('noreply@whyjustrun.ca' => 'WhyJustRun'));
-        $email->to(Configure::read('Log.Emails'));
-        $email->subject('WhyJustRun '.$type);
-        
-        $message = "
+        $emails = Configure::read('Log.Emails');
+        if (count($emails) > 0) {
+            $email = new CakeEmail('default');
+            $email->emailFormat('text');
+            $email->from(array('noreply@whyjustrun.ca' => 'WhyJustRun'));
+            $email->to($emails);
+            $email->subject('WhyJustRun '.$type);
+
+            $message = "
 URL: ".$_SERVER['REQUEST_URI']."
 Method: ".$_SERVER['REQUEST_METHOD']."
 
 ";
-        $message .= print_r(getallheaders(), true);
-        $message .= "
-        
-        ";
-        $message .= print_r($GLOBALS, true);
-        $message .= "
-        
-        ";
-        $message .= $error;
-        $email->send($message);
+            $message .= print_r(getallheaders(), true);
+            $message .= "
+
+            ";
+            $message .= print_r($GLOBALS, true);
+            $message .= "
+
+            ";
+            $message .= $error;
+            $email->send($message);
+        }
     }
 }
