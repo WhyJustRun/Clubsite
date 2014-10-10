725028b6dc853eeba72f241542d2c74e12bc8d56
Russell Porter <contact@russellporter.com>
2014-10-10 21:08:29 +0000

    Reduce false-positive email volume

--- app/Lib/Log/Engine/EmailLog.php
+++ app/Lib/Log/Engine/EmailLog.php
@@ -9,34 +9,38 @@ class EmailLog implements CakeLogInterface {
 
     // Reduce the email volume
     function shouldEmail($type, $error) {
-        return (strpos($error, '[MissingControllerException]') === false) &&
-               (strpos($error, 'SMTP Error: 421 4.7.0 Temporary System Problem') === false);
+        return
+            (strpos($error, '[MissingControllerException]') === false) &&
+            (strpos($error, '[MissingActionException]') === false) &&
+            (strpos($error, 'SMTP Error: 421 4.7.0 Temporary System Problem') === false);
     }
 
     function write($type, $error) {
-        $emails = Configure::read('Log.Emails');
-        if (count($emails) > 0) {
-            $email = new CakeEmail('default');
-            $email->emailFormat('text');
-            $email->from(array('noreply@whyjustrun.ca' => 'WhyJustRun'));
-            $email->to($emails);
-            $email->subject('WhyJustRun '.$type);
+        if ($this->shouldEmail($type, $error)) {
+            $emails = Configure::read('Log.Emails');
+            if (count($emails) > 0) {
+                $email = new CakeEmail('default');
+                $email->emailFormat('text');
+                $email->from(array('noreply@whyjustrun.ca' => 'WhyJustRun'));
+                $email->to($emails);
+                $email->subject('WhyJustRun '.$type);
 
-            $message = "
-URL: ".$_SERVER['REQUEST_URI']."
-Method: ".$_SERVER['REQUEST_METHOD']."
+                $message = "
+    URL: ".$_SERVER['REQUEST_URI']."
+    Method: ".$_SERVER['REQUEST_METHOD']."
 
-";
-            $message .= print_r(getallheaders(), true);
-            $message .= "
+    ";
+                $message .= print_r(getallheaders(), true);
+                $message .= "
 
-            ";
-            $message .= print_r($GLOBALS, true);
-            $message .= "
+                ";
+                $message .= print_r($GLOBALS, true);
+                $message .= "
 
-            ";
-            $message .= $error;
-            $email->send($message);
+                ";
+                $message .= $error;
+                $email->send($message);
+            }
         }
     }
 }
