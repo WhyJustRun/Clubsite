757fc10d7dd133a1120ebeee64b9324125c79af9
Russell Porter <contact@russellporter.com>
2018-11-28 06:32:07 +0000

    Tweak icon anchoring

--- app/webroot/js/wjr/map.js
+++ app/webroot/js/wjr/map.js
@@ -127,7 +127,7 @@ define(['jquery', 'underscore', 'async!https://maps.googleapis.com/maps/api/js?k
                       path: "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z",
                       fillColor: color,
                       fillOpacity: 1.0,
-                      anchor: new google.maps.Point(0,0),
+                      anchor: new google.maps.Point(12, 24),
                       strokeOpacity: 0.5,
                       strokeWeight: 2.0,
                       strokeColor: '#000000',
