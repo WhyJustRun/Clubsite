9fe5d2d0d37476a52679f321d864fe84a2eb07af
Russell Porter <russell.porter@autoscout24.com>
2021-11-02 11:46:04 +0900

    Add another db reconnect workaround

--- src/app/Console/Command/Task/UseClubTask.php
+++ src/app/Console/Command/Task/UseClubTask.php
@@ -2,6 +2,9 @@
 class UseClubTask extends Shell {
     public $uses = array('Club');
     public function execute($id) {
+        // Workaround for CakePHP not recreating the db connection if it times out on a long script run.
+		$this->Club->getDatasource()->reconnect();
+
         useClubWithId($id);
     }
 }
